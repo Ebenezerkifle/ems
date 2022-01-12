c7ada71dcdda846d756cfa95d9c77bf2924ed87c
Ebenezerkifle <49494895+Ebenezerkifle@users.noreply.github.com>
2022-01-12 21:35:17 +0300

    Home screen rough edit

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -10,6 +10,87 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Finance")),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Todo list progress")),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Employee progress")),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Tasks")),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("My Attendance")),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Location")),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Chat")),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white60,
+                    textStyle: const TextStyle(fontSize: 14.0),
+                  ),
+                  child: const Text("Employees Info")),
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
