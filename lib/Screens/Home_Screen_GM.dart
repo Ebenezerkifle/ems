385471aa1add946513656467d21e403a73118228
Anwar Dessie <anwarkedir89@gmail.com>
2022-01-15 09:00:13 +0300

    I have made changes on the GM home page

---
+++ lib/Screens/Home_Screen_GM.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreenGM extends StatefulWidget {
+  const HomeScreenGM({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenGMState createState() => _HomeScreenGMState();
+}
+
+class _HomeScreenGMState extends State<HomeScreenGM> {
+  @override
+  Widget build(BuildContext context) {
+    // to get size
+    var size = MediaQuery.of(context).size;
+    //style
+    var cardTextStyle = TextStyle(
+      fontFamily: 'Montserat Regular',
+      fontSize: 14,
+      color: Color.fromRGBO(63, 63, 63, 1),
+    );
+
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Container(
+          height: size.height * .3,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  alignment: Alignment.topCenter,
+                  image: AssetImage('assets/images/mobile_1.png'),
+                  fit: BoxFit.fill)),
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 64,
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const CircleAvatar(
+                        radius: 32,
+                        backgroundImage: NetworkImage(
+                            'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png'),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          const Text(
+                            'Anwar Kedir',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat Medium',
+                                fontSize: 18,
+                                color: Colors.white),
+                          ),
+                          const Text(
+                            '14806798',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontFamily: 'Montserrat Regular'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    primary: false,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      //Card 1
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            /*  SvgPicture.network(
+                              'https://www.svgrepo.com/show/125846/graduate.svg',
+                              height: 128,
+                            ), */
+                            SvgPicture.asset(
+                              'assets/images/finance.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Finance',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 2
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/todo.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Todo List Progress',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 3
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/emp-progress.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Employee Progress',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 4
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/tasks.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Tasks',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 5
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/calendar.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'My Attendance',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 6
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/location.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Location',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 7
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/chat.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Chat',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Card 8
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/images/info.svg',
+                              height: 64,
+                            ),
+                            Text(
+                              'Employee Info',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:ems/Screens/Login_Screen.dart';
+//import 'package:ems/Screens/Login_Screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'package:ems/Screens/Home_Screen_GM.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
   // entry point of an application.
   // inflates the widget and show it on app screen.
   runApp(const MyApp());
@@ -16,7 +17,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Login(),
+      debugShowCheckedModeBanner: false,
+      // home: Login(),
+      home: HomeScreenGM(),
     );
   }
 }
