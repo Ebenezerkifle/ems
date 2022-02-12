671a0446b05b647c178fad74b12e54130ed63d62
Ebenezerkifle <49494895+Ebenezerkifle@users.noreply.github.com>
2022-02-13 01:46:41 +0300

    Task page
    Task page was previously designed to show the overview and to show detail  of a task in addition to creating new task. but it is now only have a functionality of showing the overview of a task.

--- lib/Screens/TaskPage.dart
+++ lib/Screens/TaskPage.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ems/Screens/SharedScreens/ChatPage.dart';
+import 'package:ems/Screens/SharedScreens/createNewTask.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -111,7 +111,12 @@ class _TaskPageState extends State<TaskPage> {
                 ),
                 child: IconButton(
                   onPressed: () {
-                    _createnewTask(context);
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            CreateTask(receiverEmail, taskDocId),
+                      ),
+                    );
                   },
                   color: Colors.white,
                   icon: const Icon(
@@ -180,7 +185,6 @@ class _TaskPageState extends State<TaskPage> {
                 children: snapshot.data!.docs.map((DocumentSnapshot document) {
                   Map<String, dynamic> data =
                       document.data()! as Map<String, dynamic>;
-
                   return _tasksItem(
                     title: data['title'],
                     description: data['description']!,
@@ -193,13 +197,13 @@ class _TaskPageState extends State<TaskPage> {
     );
   }
 
-  Widget _tasksItem({required String title, description, time}) {
+  Widget _tasksItem({required String title, description, var time}) {
     return InkWell(
       onTap: () {
         Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) =>
-                TaskDetail(receiverEmail, title, taskDocId, time),
+                TaskDetail(taskDocId, receiverEmail, description, title, time),
           ),
         );
       },
@@ -256,81 +260,6 @@ class _TaskPageState extends State<TaskPage> {
       ),
     );
   }
-
-  void _createnewTask(context) {
-    TextEditingController titleController = TextEditingController();
-    TextEditingController descirptionController = TextEditingController();
-    String title;
-    String description;
-    showDialog(
-        context: context,
-        builder: (BuildContext builder) {
-          return Material(
-              child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.65,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 5, vertical: 20),
-                      child: Text("New Task",
-                          style: TextStyle(
-                              fontSize: 40.0, color: Colors.blueAccent))),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  ),
-                  TextFormField(
-                    controller: titleController,
-                    onChanged: (value) => {setState(() => title = value)},
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                      hintText: "Title",
-                    ),
-                  ),
-                  TextFormField(
-                    // validator: (value) => value!.length < 6
-                    //     ? "Enter 6+ chars for password"
-                    //     : null,
-                    controller: descirptionController,
-                    onChanged: (value) => {setState(() => description = value)},
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                      hintText: "Description...",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                    child: InkWell(
-                      onTap: () {
-                        if (titleController.text.isNotEmpty &&
-                            descirptionController.text.isNotEmpty) {
-                          tasks.doc(taskDocId).collection('Tasks').add({
-                            "title": titleController.text.trim(),
-                            "description": descirptionController.text.trim(),
-                            "creator": loginUserEmail?.trim(),
-                            "assignedTo": receiverEmail.trim(),
-                            "timeStamp": DateTime.now()
-                            //"due_date":
-                          });
-                          descirptionController.clear();
-                          titleController.clear();
-                          Navigator.pop(context);
-                        }
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.indigoAccent,
-                        child: Text('Send'),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ));
-        });
-  }
 }
 
 class Avatar extends StatelessWidget {
@@ -351,224 +280,6 @@ class Avatar extends StatelessWidget {
             image: AssetImage(image),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class TaskDetail extends StatefulWidget {
-  String receiverEmail;
-  String title;
-  String taskDocId;
-  String timeStamp;
-  TaskDetail(this.receiverEmail, this.title, this.taskDocId, this.timeStamp,
-      {Key? key})
-      : super(key: key);
-
-  @override
-  _TaskDetailState createState() =>
-      _TaskDetailState(receiverEmail, title, taskDocId, timeStamp);
-}
-
-class _TaskDetailState extends State<TaskDetail> {
-  String receiverEmail;
-  String title;
-  String taskDocId;
-  String timeStamp;
-  _TaskDetailState(
-      this.receiverEmail, this.title, this.taskDocId, this.timeStamp);
-
-  CollectionReference tasks = FirebaseFirestore.instance.collection("Tasks");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                _topTask(),
-                _bodyTask(),
-                const SizedBox(
-                  height: 120,
-                )
-              ],
-            ),
-            // _formTask(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _topTask() {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              GestureDetector(
-                onTap: () => Navigator.of(context).pop(),
-                child: const Icon(
-                  Icons.arrow_back_ios,
-                  size: 25,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                title,
-                style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.black12,
-                ),
-                child: const Icon(
-                  Icons.call,
-                  size: 25,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Container(
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.black12,
-                ),
-                child: const Icon(
-                  Icons.videocam,
-                  size: 25,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _bodyTask() {
-    return Expanded(
-      child: Container(
-          padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(45), topRight: Radius.circular(45)),
-            color: Colors.white,
-          ),
-          child: Column(
-            children: [
-              Container(),
-              StreamBuilder<QuerySnapshot>(
-                stream: tasks
-                    .doc(taskDocId)
-                    .collection("Tasks")
-                    .doc(taskDocId)
-                    .collection(timeStamp)
-                    .orderBy('timeStamp')
-                    .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    Fluttertoast.showToast(msg: "Error occured");
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  return ListView(
-                    padding: const EdgeInsets.only(top: 35),
-                    physics: const BouncingScrollPhysics(),
-                    children:
-                        snapshot.data!.docs.map((DocumentSnapshot document) {
-                      Map<String, dynamic> data =
-                          document.data()! as Map<String, dynamic>;
-                      int sender =
-                          0; //by default we assume sender is current user.
-                      if (data['user'].toString() == receiverEmail) {
-                        sender = 1;
-                      }
-                      return _taskItems(
-                        chat: sender,
-                        message: data['msg']!,
-                        time: '08.00',
-                      );
-                    }).toList(),
-                  );
-                },
-              ),
-            ],
-          )),
-    );
-  }
-
-  Widget _taskItems({int? chat, String? message, String? time}) {
-    return Container();
-  }
-
-  Widget _formChat() {
-    //var messageController;
-    return Positioned(
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-            height: 120,
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            color: Colors.white,
-            child: Row(
-              children: [
-                Flexible(
-                  child: TextField(
-                    //controller: messageController,
-                    decoration: InputDecoration(
-                      hintText: 'Type your message...',
-                      filled: true,
-                      fillColor: Colors.blueGrey[50],
-                      labelStyle: const TextStyle(fontSize: 12),
-                      contentPadding: const EdgeInsets.all(20),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.blueGrey),
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.blueGrey),
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    // _sendMessage();
-                  },
-                  color: Colors.blueAccent,
-                  icon: const Icon(
-                    Icons.send_rounded,
-                    color: Colors.blue,
-                    size: 28,
-                  ),
-                )
-              ],
-            )),
       ),
     );
   }
